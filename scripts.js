d489227510b66b839c6f1df0cc1e89e8bd24c578
Reyes Espinosa <andrea.reyes@akzonobel.com>
2018-07-06 19:30:09 -0300

    https://github.com/AndyReyesEspinosa/Desafio1Programador.git

---
+++ scripts.js
@@ -0,0 +1,67 @@
+//for ( var i=1; i<=250; ++i) {
+//	console.log(i)
+//}
+
+//var i=10;
+//while ( i<=1000) {
+
+//if (i%3==0) {
+
+//	console.log(i)
+//}
+//i++
+//}
+
+
+//for (var i=12; i<1000; i=i+3) {
+
+//	console.log(i)
+
+//}
+
+//ESCRIBIR HACIA ADELANTE
+var a="Anita lava la tina";
+
+for ( var i=0; i<a.length; i++) {
+
+	console.log(a[i])
+
+}
+
+//VOLVER PARA ATRAS
+var a="Anita lava la tina";
+
+for ( var i=a.length; i>-1; i--) {
+
+	console.log(a[i])
+
+}
+// para recortar espacios de una cadena
+//" Hola como estas?? ".replace(/ /g, '');
+
+//var saludo= prompt ("Cual es tu Nombre?")
+
+
+
+//if (saludo!="") {
+
+//	alert("Hola"+ " " + saludo)
+
+//}
+
+//var nombre= prompt("Cual es tu nombre")
+//var edad= prompt("Cuantos años tenes?")
+
+//if(edad>20) {
+
+//	alert("Que tengas un Lindo Dia!!")
+
+//} else if (edad>=5 && edad<20) {
+
+//	alert("Que la pases faluero hoy :0")
+
+//} else if (edad<5) {
+
+//	alert("Volve mañana please!!")
+//}
+
